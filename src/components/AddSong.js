92fff9635e54de79b802c0b9e5ba18317176ca73
J-David-Baxter <jdavidbx@gmail.com>
2023-01-23 11:59:14 -0500

    Add dialog on add song

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,7 +1,70 @@
-import React from "react";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, InputAdornment, TextField } from "@material-ui/core";
+import { AddBoxOutlined, Link } from "@material-ui/icons";
+import React, { useState } from "react";
 
 function AddSong() {
-  return <div>addsong</div>;
+  const [dialog, setDialog] = useState(false);
+
+  function handleCloseDialog() {
+    setDialog(false);
+  }
+   
+  return (
+    <div>
+      <Dialog 
+        open={dialog}
+        onClose={handleCloseDialog}
+        >
+        <DialogTitle>Edit Song</DialogTitle>
+        <DialogContent>
+          <img />
+          <TextField 
+            margin="dense"
+            name="title"
+            label="Title"
+            fullWidth
+          />
+          <TextField 
+            margin="dense"
+            name="artist"
+            label="Artist"
+            fullWidth
+          />
+          <TextField 
+            margin="dense"
+            name="thumbnail"
+            label="Thumbnail"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="secondary">Cancel</Button>
+          <Button variant="outlined" color="primary">Add Song</Button>
+        </DialogActions>
+      </Dialog>
+      <TextField 
+        placeholder="Add Youtube or Soundcloud Url"
+        fullWidth
+        margin="normal"
+        type="url"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment>
+              <Link />
+            </InputAdornment>
+          )
+        }}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        endIcon={<AddBoxOutlined />}
+        onClick={() => setDialog(true)}
+      >
+        Add
+      </Button>
+    </div>
+  )
 }
 
 export default AddSong;

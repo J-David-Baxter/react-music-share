4b41c415add0fbd5d50082249d136f497e417fc3
J-David-Baxter <jdavidbx@gmail.com>
2023-01-23 12:43:23 -0500

    Style song card

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import { Card, CardActions, CardContent, CardMedia, CircularProgress, IconButton, Typography } from "@material-ui/core";
+import { Card, CardActions, CardContent, CardMedia, CircularProgress, IconButton, makeStyles, Typography } from "@material-ui/core";
 import { PlayArrow, Save } from "@material-ui/icons";
 import React from "react";
 
@@ -29,13 +29,34 @@ function SongList() {
   ))}</div>;
 }
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    margin: theme.spacing(3)
+  },
+  songInfoContainer: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  songInfo: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  thumbnail: {
+    objectFit: 'cover',
+    width: 140,
+    height: 140
+  }
+}))
+
 function Song({ song }) {
   const { thumbnail, title, artist } = song;
-  
-  return <Card>
-    <div>
-      <CardMedia image={thumbnail}/>
-      <div>
+  const classes = useStyles();
+
+  return <Card className={classes.container}>
+    <div className={classes.songInfoContainer}>
+      <CardMedia className={classes.thumbnail} image={thumbnail}/>
+      <div className={classes.songInfo}>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {title}
